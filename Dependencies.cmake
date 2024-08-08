7feb39376879e06ece55840b173ff9b332e4689c
Salavat Uzbekov <scrblmer@gmail.com>
2024-08-08 21:57:30 +0300

    Updated libraries

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -25,11 +25,11 @@ function(TimeLine_setup_dependencies)
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    cpmaddpackage("gh:catchorg/Catch2@3.6.0")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
+    cpmaddpackage("gh:CLIUtils/CLI11@2.4.2")
   endif()
 
   if(NOT TARGET ftxui::screen)
